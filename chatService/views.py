44adbe16d2e3771955d20e7765f3572d021f288b
HamishSalisbury <Hamishsali3@gmail.com>
2026-07-15 18:44:34 +1200

    feat: health endpoint

--- chatService/views.py
+++ chatService/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def health_check(request):
+    """
+    Health check endpoint to verify that the service is running.
+    """
+    return Response({"status": "ok"})
