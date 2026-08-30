53f4579b4cd7995b34b697efaf8d0a5aabc623d9
HamishSalisbury <Hamishsali3@gmail.com>
2026-08-30 12:33:07 +1200

    feat: chunks are only returned if they are above min_cosine_similiarty threshold

--- chatService/services.py
+++ chatService/services.py
@@ -4,6 +4,7 @@ from sentence_transformers import SentenceTransformer
 import sys
 from config.settings import RULES_TOP_K
 from config.settings import PATH_TO_INDEX
+from config.settings import MIN_COSINE_SIMILIARITY
 def query_chatbot(query):
 
     client = chromadb.PersistentClient(path=PATH_TO_INDEX)
@@ -25,6 +26,15 @@ def query_chatbot(query):
     query_embedding = model.encode([query], normalize_embeddings=True)
 
     res = collection.query(query_embeddings=query_embedding.tolist(), n_results=RULES_TOP_K)
+    docs = res["documents"][0]
+    dists = res["distances"][0]
+    metas = res["metadatas"][0]
 
-    return res
+    keep = [i for i, dist in enumerate(dists) if (1 - dist) >= MIN_COSINE_SIMILIARITY]
+
+    res["documents"][0] = [docs[i] for i in keep]
+    res["distances"][0] = [dists[i] for i in keep]
+    res["metadatas"][0] = [metas[i] for i in keep]
+
+    return res  
 
