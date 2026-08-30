bc284c2e2d7fddb08c254dc635343a698548cf3d
HamishSalisbury <Hamishsali3@gmail.com>
2026-08-30 17:27:23 +1200

    test: testing llm acceptance critiera

--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,15 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+load_dotenv(BASE_DIR / ".env")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*m8kem58-u2x(gkj8n&%ue@2$*=8gf=yy_d6j2y@3dw^^+dzh('
